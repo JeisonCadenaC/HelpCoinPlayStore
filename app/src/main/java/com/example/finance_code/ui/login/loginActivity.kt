789aa57b56016d95aed94ad1e9c9c86b9db46532
Zipa008 <santiagozipa2805@gmail.com>
2025-08-21 12:58:23 -0500

    LOGIN FUNCIONAL: CAMBIOS A HACER: VALIDAR QUE NO PASE A LA OTRA INTERFAZ SI ES UN USUARIO NUEVO, OSEA CONFIGURAR EL BOTON DE REGISTRARSE, QUE SI LE DA A INCIAR SESION Y ES UN USUARIO NUEVO NO LO DEJE PARA QUE LE TENGA QUE DAR A REGISTRARSE

---
+++ app/src/main/java/com/example/finance_code/ui/login/loginActivity.kt
@@ -0,0 +1,72 @@
+package com.example.finance_code.ui.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import com.example.finance_code.R
+import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
+import com.example.finance_code.ui.main.MainActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    // Declare view variables at the class level
+    private lateinit var singUpButton: Button
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setup()
+    }
+
+    private fun setup() {
+        title = "Autenticación del usuario"
+        singUpButton = findViewById(R.id.btnsingUpButton)
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+
+        singUpButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    emailEditText.text.toString(),
+                    passwordEditText.text.toString()
+                ).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Navega a la vista principal si la autenticación es exitosa
+                        showPrincipalView()
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showPrincipalView() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
