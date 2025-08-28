3f95f59d1cf461eab9228c7e52694e655de57bfe
Zipa008 <santiagozipa2805@gmail.com>
2025-08-27 21:06:28 -0500

    Commit 2 Zipa:  lOGIN CON VERIFICACION DE INICIO DE SESION CON CORREO Y CONTRASEÑA Y CON GOOGLE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,16 +39,31 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.appcompat)
+
     implementation(libs.material)
+
     implementation(libs.androidx.activity)
+
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
+
     androidTestImplementation(libs.androidx.espresso.core)
+
     implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+
     implementation("com.google.firebase:firebase-analytics")
-    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+
     implementation("com.google.firebase:firebase-auth")
+
+    implementation("androidx.credentials:credentials:1.3.0")
+
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
 
 }
